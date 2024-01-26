9b51b2c4f291b02400014ca646486a417d309d00
Zywl <86253343+opZywl@users.noreply.github.com>
2024-01-26 19:09:54 -0300

    descrição / copyright

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+/*
+ * Este arquivo faz parte do projeto PetModel (https://github.com/opZywl/PetModel-Backend)
+ * 
+ * Copyright (c) 2024 - 2024 opZywl
+ * 
+ * Certifique-se de estar com o servidor frontend em execução antes de iniciar o backend para garantir
+ * que as chamadas à API possam ser processadas corretamente. (https://github.com/opZywl/PetModel-Frontend)
+ *
+ * Este projeto foi desenvolvido como parte do Desafio da NectarLabs para o controle de adoção de pets por uma ONG.
+ */
 const express = require('express');
 const petRouter = require('./router/petRouter');
 const authRouter = require('./router/authRouter');
